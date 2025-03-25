7621427be30bb0644b87f967255ca5ea5d43e4f8
saadetsimsek <saadetsimsek13@gmail.com>
2025-03-25 15:06:31 +0300

    collectionViewCell object

--- DropDownCell/Cell/QuestionCollectionViewCell.swift
+++ DropDownCell/Cell/QuestionCollectionViewCell.swift
@@ -18,18 +18,48 @@ class QuestionCollectionViewCell: UICollectionViewCell {
         return view
     }()
     
+    lazy var questionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        label.textColor = .purple
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var checkImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.widthAnchor.constraint(equalToConstant: 46).isActive = true
+        image.heightAnchor.constraint(equalToConstant: 46).isActive = true
+        return image
+    }()
+    
     lazy var cellContent: UIView = {
         let view = UIView()
         view.backgroundColor = .white
+        view.addSubview(itemStackView)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
+    lazy var itemStackView : UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 9
+        stack.alignment = .fill
+        stack.distribution = .fill
+        return stack
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(cellHeader)
+      
         clipsToBounds = true
         layer.cornerRadius = 20
+        
+        contentView.addSubview(cellHeader)
+        contentView.addSubview(cellContent)
     }
     
     required init?(coder: NSCoder) {
